83c5eee2799a79a95a7b3994d01eda6b497bf71e
Olivier Friard <olivier.friard@unito.it>
2019-02-28 15:27:37 +0100

    upgrade opencv to 4.0

--- src/doris_functions.py
+++ src/doris_functions.py
@@ -305,7 +305,7 @@ def detect_and_filter_objects(frame,
     """
 
     #_, contours, _ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    _, contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     nr_objects = len(contours)
 
